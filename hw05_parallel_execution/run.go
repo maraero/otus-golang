aabc6b0378c09b6f122b7986b2226110feaeefce
Evgeny Kudryavtsev <evgeniy.kudriavtsev@sravni.ru>
2021-10-16 22:48:55 +0300

    add base imlementation for Run function

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,52 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+func handleTask(chTasks chan Task, chErrors chan error, chDone chan struct{}, wg *sync.WaitGroup) {
+	for {
+		select {
+		case <-chDone:
+			wg.Done()
+			return
+		case t := <-chTasks:
+			if err := t(); err != nil {
+				chErrors <- err
+			}
+		}
+	}
+}
+
+func Run(tasks []Task, n int, m int) error {
+	chTasks := make(chan Task)
+	chErrors := make(chan error, len(tasks))
+	chDone := make(chan struct{})
+
+	wg := sync.WaitGroup{}
+
+	defer func() {
+		close(chDone)
+		wg.Wait()
+	}()
+
+	wg.Add(n)
+
+	for i := 1; i <= n; i++ {
+		go handleTask(chTasks, chErrors, chDone, &wg)
+	}
+
+	for _, t := range tasks {
+		chTasks <- t
+
+		if m > 0 && len(chErrors) >= m {
+			return ErrErrorsLimitExceeded
+		}
+	}
+
 	return nil
 }
