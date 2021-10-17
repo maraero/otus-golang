ec2c3927ce99750e4f9c8ae4b91784df60c532c7
Evgeny Kudryavtsev <evgeniy.kudriavtsev@sravni.ru>
2021-10-17 15:07:23 +0300

    fix linter and delete repeating test

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -15,7 +15,7 @@ import (
 func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
-	t.Run("if errors happened in first M tasks, then finished not more than N+M tasks, where N is workers amount", func(t *testing.T) {
+	t.Run("if errors are in first M tasks, then finished not more than N+M tasks", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
 
@@ -38,30 +38,7 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
-	t.Run("if errors happened in first M tasks, then finished not more than N+M tasks, where N is workers amount", func(t *testing.T) {
-		tasksCount := 50
-		tasks := make([]Task, 0, tasksCount)
-
-		var runTasksCount int32
-
-		for i := 0; i < tasksCount; i++ {
-			err := fmt.Errorf("error from task %d", i)
-			tasks = append(tasks, func() error {
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
-				atomic.AddInt32(&runTasksCount, 1)
-				return err
-			})
-		}
-
-		workersCount := 10
-		maxErrorsCount := 23
-		err := Run(tasks, workersCount, maxErrorsCount)
-
-		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
-		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
-	})
-
-	t.Run("if were less errors than max errors amount, then finished all the tasks", func(t *testing.T) {
+	t.Run("if errors < max errors amount, then finished all the tasks", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
 
@@ -73,7 +50,7 @@ func TestRun(t *testing.T) {
 				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
 				atomic.AddInt32(&runTasksCount, 1)
 
-				if i == 10 || i == 20 || i == 30 { // 3 errors in 50 tasks
+				if i == 10 || i == 20 || i == 30 { // 3 errors, max — 4
 					return err
 				}
 
